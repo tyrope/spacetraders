0a79f334ad235c92d3e9e1b73f3d2d7be50e61d9
Tyrope <tyrope@tyrope.nl>
2023-05-17 21:33:51 +0200

    Ship UI!

--- Assets/Scripts/HudManager.cs
+++ Assets/Scripts/HudManager.cs
@@ -1,3 +1,4 @@
+﻿using System;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
@@ -19,6 +20,8 @@ namespace SpaceTraders
         private RectTransform FleetTransform;
         private readonly Dictionary<string, GameObject> ShipGOs = new Dictionary<string, GameObject>();
 
+        private float timeSinceLastUpdate = Mathf.Infinity;
+        private readonly float timeBetweenUpdates = 1f;
         // Start is called before the first frame update
         void Start() {
             AgentInfoDisplay = HUD.transform.Find("AgentInfo").GetComponent<TMP_Text>();
@@ -27,10 +30,15 @@ namespace SpaceTraders
 
         // Update is called once per frame
         void Update() {
-            UpdateAgentInfo();
+            // Rate limit
+            if(timeSinceLastUpdate < timeBetweenUpdates) {
+                timeSinceLastUpdate += Time.deltaTime;
+                return;
+            }
+            timeSinceLastUpdate = 0;
 
-            // TODO BROKEN!
-            //UpdateFleetHUD();
+            UpdateAgentInfo();
+            UpdateFleetHUD();
         }
 
         private void OnDestroy() {
@@ -63,8 +71,27 @@ namespace SpaceTraders
             Ship ship = await shipManager.GetShip(shipSymbol);
             trans.Find("Registration").GetComponent<TMP_Text>().text = ship.registration.name;
             trans.Find("Role").GetComponent<TMP_Text>().text = ship.registration.role.ToString();
-            trans.Find("Cargo").GetComponent<TMP_Text>().text = $"Cargo: {(ship.cargo.units/ship.cargo.capacity):n2}%\n{ship.cargo.units}/{ship.cargo.capacity}";
-            trans.Find("Fuel").GetComponent<TMP_Text>().text = $"Fuel: {(ship.fuel.current / ship.fuel.capacity):n2}%\n{ship.fuel.current}/{ship.fuel.capacity}";
+            trans.Find("Cargo").GetComponent<TMP_Text>().text = $"Cargo: {ship.cargo.units / (float) ship.cargo.capacity * 100f:n2}%\n{ship.cargo.units}/{ship.cargo.capacity}";
+            trans.Find("Fuel").GetComponent<TMP_Text>().text = $"Fuel: {ship.fuel.current / (float) ship.fuel.capacity * 100f:n2}%\n{ship.fuel.current}/{ship.fuel.capacity}";
+
+            string navString;
+            switch(ship.nav.status) {
+                case Ship.Navigation.Status.DOCKED:
+                    navString = $"DOCKED @ {ship.nav.waypointSymbol}";
+                    break;
+                case Ship.Navigation.Status.IN_ORBIT:
+                    navString = $"ORBITING {ship.nav.waypointSymbol}";
+                    break;
+                case Ship.Navigation.Status.IN_TRANSIT:
+                    DateTime ArrivalTime = DateTime.Parse(ship.nav.route.arrival);
+                    string ETA = (ArrivalTime - DateTime.Now).ToString("HH:mm:ss");
+                    navString = $"{ship.nav.route.departure}→{ship.nav.route.destination} ({ETA})";
+                    break;
+                default:
+                    navString = "ERR_INVALID_NAV_STATUS";
+                    break;
+            }
+            trans.Find("Navigation").GetComponent<TMP_Text>().text = navString;
         }
 
         private GameObject SpawnShip(string symbol) {
