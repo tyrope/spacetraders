6032f33e2d4983da47c76a305640ffa878738f0e
Tyrope <tyrope@tyrope.nl>
2023-05-24 22:55:57 +0200

    I'm clearly too tired from work to do this; but at least orbitals aren't overlapping with their parent anymore.

--- Assets/Scripts/Components/WaypointVisual.cs
+++ Assets/Scripts/Components/WaypointVisual.cs
@@ -1,3 +1,4 @@
+using System;
 using TMPro;
 using UnityEngine;
 
@@ -10,19 +11,47 @@ namespace STCommander
         public Waypoint waypoint;
         public GameObject[] models;
 
+        private Transform parentOrbit = null;
+        private int totalSatellites = 1;
+        private int ourSatelliteIndex = 0;
+        private string WaypointSymbolEnd => waypoint.symbol.Split('-')[2];
+
         private float OrbitalAltitude;
-        private float OrbitalPeriod => Mathf.Sqrt((4 * Mathf.Pow(Mathf.PI, 2) * Mathf.Pow(OrbitalAltitude, 3)) / 6.67430e-11f)/1000f;
+        private float OrbitalPeriod => Mathf.Sqrt((4 * Mathf.Pow(Mathf.PI, 2) * Mathf.Pow(parentOrbit == null ? OrbitalAltitude * solarSystem.GetSystemScale() : OrbitalAltitude, 3)) / 6.67430e-11f) / 1000f;
         private float OrbitTime;
 
         // Start is called before the first frame update
         void Start() {
             Instantiate(models[(int) waypoint.type], transform.Find("Visuals"));
-            gameObject.name = $"({waypoint.x},{waypoint.y}){waypoint.symbol.Split('-')[2]}";
-            gameObject.GetComponentInChildren<TMP_Text>().text = waypoint.symbol.Split('-')[2];
-            OrbitalAltitude = new Vector2(waypoint.x, waypoint.y).magnitude * solarSystem.GetSystemScale();
+            gameObject.name = $"({waypoint.x},{waypoint.y}){WaypointSymbolEnd}";
+            gameObject.GetComponentInChildren<TMP_Text>().text = WaypointSymbolEnd;
 
-            OrbitTime = System.DateTime.Now.Ticks % OrbitalPeriod;
+            // Are we orbiting the main star, or another waypoint?
+            SetParentOrbit();
+            if(parentOrbit == null) {
+                OrbitalAltitude = new Vector2(waypoint.x, waypoint.y).magnitude;
+                OrbitTime = DateTime.Now.Ticks % OrbitalPeriod;
+            } else {
+                OrbitalAltitude = 2f;
+                // TODO Fix all satellites overlapping despite being offset by Index.
+                OrbitTime = DateTime.Now.Ticks + OrbitalPeriod * (ourSatelliteIndex / (float) totalSatellites) % OrbitalPeriod;
+            }
             SetPosition();
+        }
+
+        private void SetParentOrbit() {
+            foreach(Waypoint wp in solarSystem.waypoints) {
+                if(wp.orbitals != null) {
+                    for(int i = 0; i < wp.orbitals.Length; i++) {
+                        if(wp.orbitals[i].symbol == waypoint.symbol) {
+                            parentOrbit = transform.parent.Find($"({wp.x},{wp.y}){wp.symbol.Split('-')[2]}");
+                            totalSatellites = wp.orbitals.Length;
+                            ourSatelliteIndex = i;
+                            return;
+                        }
+                    }
+                }
+            }
         }
 
         private void Update() {
@@ -32,10 +61,20 @@ namespace STCommander
         }
 
         public void SetPosition() {
-            float rotation = (OrbitTime / OrbitalPeriod) * 360f;
-            float xPos = OrbitalAltitude * Mathf.Sin(rotation);
-            float yPos = OrbitalAltitude * Mathf.Cos(rotation);
-            transform.position = new Vector3(xPos, 0, yPos);
+            float rot = (OrbitTime / OrbitalPeriod) * 360f;
+            if(parentOrbit == null) {
+                // We're orbiting the main star; align around 0.
+                transform.position = new Vector3(
+                    OrbitalAltitude * solarSystem.GetSystemScale() * Mathf.Sin(rot), 0,
+                    OrbitalAltitude * solarSystem.GetSystemScale() * Mathf.Cos(rot));
+            } else {
+                // We're orbiting another body; align around them.
+                // TODO These don't actually orbit, they're just static relative to their parent.
+                transform.position = parentOrbit.position +
+                    new Vector3(
+                        OrbitalAltitude * solarSystem.GetSystemScale() * Mathf.Sin(rot), 0,
+                        OrbitalAltitude * solarSystem.GetSystemScale() * Mathf.Cos(rot));
+            }
         }
     }
 }
