63e86e63a3a0d465af5469d62d31f9a802c8dda2
Tyrope <tyrope@tyrope.nl>
2023-06-09 12:19:21 +0200

    Apparently it's important to use INTEGER and not INT for SQLite?

--- Assets/Resources/stCommanderSchema.sql
+++ Assets/Resources/stCommanderSchema.sql
@@ -3,29 +3,29 @@ CREATE TABLE IF NOT EXISTS Agent (
     accountId TEXT NOT NULL,
     symbol TEXT NOT NULL PRIMARY KEY,
     headquarters TEXT NOT NULL REFERENCES Waypoint(symbol),
-    credits INT NOT NULL,
+    credits INTEGER NOT NULL,
     startingFaction TEXT NOT NULL REFERENCES Faction(symbol),
-    lastEdited INT NOT NULL
+    lastEdited INTEGER NOT NULL
 );
 CREATE TABLE IF NOT EXISTS Chart (
     waypointSymbol TEXT NOT NULL PRIMARY KEY REFERENCES Waypoint(symbol),
     submittedBy TEXT,
     submittedOn INTEGER,
-    lastEdited INT NOT NULL
+    lastEdited INTEGER NOT NULL
 );
 CREATE TABLE IF NOT EXISTS ConnectedSystem (
     symbol TEXT NOT NULL PRIMARY KEY REFERENCES System(symbol),
-    distance INT NOT NULL
+    distance INTEGER NOT NULL
 );
 CREATE TABLE IF NOT EXISTS Contract (
     id TEXT NOT NULL PRIMARY KEY,
     factionSymbol TEXT NOT NULL REFERENCES Faction(Symbol),
     type TEXT NOT NULL REFERENCES ContractType(value),
-    terms INT NOT NULL REFERENCES ContractTerms(rowid),
-    accepted INT NOT NULL CHECK (accepted IN (0,1)),
-    fulfilled INT NOT NULL CHECK (fulfilled IN (0,1)),
-    deadlineToAccept INT NOT NULL,
-    lastEdited INT NOT NULL
+    terms INTEGER NOT NULL REFERENCES ContractTerms(rowid),
+    accepted INTEGER NOT NULL CHECK (accepted IN (0,1)),
+    fulfilled INTEGER NOT NULL CHECK (fulfilled IN (0,1)),
+    deadlineToAccept INTEGER NOT NULL,
+    lastEdited INTEGER NOT NULL
 );
 CREATE TABLE IF NOT EXISTS ContractType (
     value TEXT NOT NULL PRIMARY KEY
@@ -33,41 +33,41 @@ CREATE TABLE IF NOT EXISTS ContractType (
 CREATE TABLE IF NOT EXISTS ContractDeliverGood (
     tradeSymbol TEXT NOT NULL REFERENCES TradeGood(symbol),
     destinationSymbol TEXT NOT NULL REFERENCES Waypoint(symbol),
-    unitsRequired INT NOT NULL,
-    unitsFulfilled INT NOT NULL
+    unitsRequired INTEGER NOT NULL,
+    unitsFulfilled INTEGER NOT NULL
 );
 CREATE TABLE IF NOT EXISTS ContractPayment (
-    onAccepted INT NOT NULL,
-    onFulfilled INT NOT NULL
+    onAccepted INTEGER NOT NULL,
+    onFulfilled INTEGER NOT NULL
 );
 CREATE TABLE IF NOT EXISTS ContractTerms (
-    deadline INT NOT NULL,
-    payment INT NOT NULL REFERENCES ContractPayment(rowid)
+    deadline INTEGER NOT NULL,
+    payment INTEGER NOT NULL REFERENCES ContractPayment(rowid)
 );
 CREATE TABLE IF NOT EXISTS ContractDeliverGood_ContractTerms_relationship (
-    good INT NOT NULL UNIQUE REFERENCES ContractDeliverGood(rowid),
-    terms INT NOT NULL REFERENCES ContractTerms(rowid)
+    good INTEGER NOT NULL UNIQUE REFERENCES ContractDeliverGood(rowid),
+    terms INTEGER NOT NULL REFERENCES ContractTerms(rowid)
 );
 CREATE TABLE IF NOT EXISTS Cooldown (
     shipSymbol TEXT NOT NULL PRIMARY KEY REFERENCES Ship(symbol),
-    totalSeconds INT NOT NULL,
-    remainingSeconds INT NOT NULL,
-    expiration INT,
-    lastEdited INT NOT NULL
+    totalSeconds INTEGER NOT NULL,
+    remainingSeconds INTEGER NOT NULL,
+    expiration INTEGER,
+    lastEdited INTEGER NOT NULL
 );
 CREATE TABLE IF NOT EXISTS Extraction (
     shipSymbol TEXT NOT NULL REFERENCES Ship(symbol),
     yieldSymbol TEXT NOT NULL REFERENCES TradeSymbol(value),
-    yieldUnits INT NOT NULL,
-    lastEdited INT NOT NULL
+    yieldUnits INTEGER NOT NULL,
+    lastEdited INTEGER NOT NULL
 );
 CREATE TABLE IF NOT EXISTS Faction (
     symbol TEXT NOT NULL PRIMARY KEY,
     name TEXT NOT NULL,
     description TEXT NOT NULL,
     headquarters TEXT NOT NULL REFERENCES System(symbol),
-    isRecruiting INT NOT NULL CHECK (isRecruiting=0 OR isRecruiting=1),
-    lastEdited INT NOT NULL
+    isRecruiting INTEGER NOT NULL CHECK (isRecruiting=0 OR isRecruiting=1),
+    lastEdited INTEGER NOT NULL
 );
 CREATE TABLE IF NOT EXISTS FactionTrait (
     symbol TEXT NOT NULL PRIMARY KEY REFERENCES FactionTraitSymbols(value),
@@ -82,17 +82,17 @@ CREATE TABLE IF NOT EXISTS FactionTrait_Faction_relationship (
     trait TEXT NOT NULL REFERENCES FactionTrait(symbol)
 );
 CREATE TABLE IF NOT EXISTS JumpGate (
-    jumpRange INT NOT NULL,
+    jumpRange INTEGER NOT NULL,
     factionSymbol TEXT REFERENCES Faction(symbol),
-    lastEdited INT NOT NULL
+    lastEdited INTEGER NOT NULL
 );
 CREATE TABLE IF NOT EXISTS ConnectedSystem_JumpGate_relationship (
-    connectedSystem INT NOT NULL REFERENCES ConnectedSystem(rowid),
-    jumpGate INT NOT NULL REFERENCES JumpGate(rowid)
+    connectedSystem INTEGER NOT NULL REFERENCES ConnectedSystem(rowid),
+    jumpGate INTEGER NOT NULL REFERENCES JumpGate(rowid)
 );
 CREATE TABLE IF NOT EXISTS Market (
     symbol TEXT NOT NULL PRIMARY KEY REFERENCES Waypoint(symbol),
-    lastEdited INT NOT NULL
+    lastEdited INTEGER NOT NULL
 );
 CREATE TABLE IF NOT EXISTS Market_exports_relationship (
     market TEXT NOT NULL REFERENCES Market(symbol),
@@ -108,7 +108,7 @@ CREATE TABLE IF NOT EXISTS Market_exchange_relationship (
 );
 CREATE TABLE IF NOT EXISTS Market_transactions_relationship (
     market TEXT NOT NULL REFERENCES Market(symbol),
-    marketTransaction INT NOT NULL UNIQUE REFERENCES MarketTransaction(rowid)
+    marketTransaction INTEGER NOT NULL UNIQUE REFERENCES MarketTransaction(rowid)
 );
 CREATE TABLE IF NOT EXISTS Market_tradegoods_relationship (
     market TEXT NOT NULL REFERENCES Market(symbol),
@@ -116,10 +116,10 @@ CREATE TABLE IF NOT EXISTS Market_tradegoods_relationship (
 );
 CREATE TABLE IF NOT EXISTS MarketTradeGood (
     symbol TEXT NOT NULL REFERENCES TradeGood(symbol),
-    tradeVolume INT NOT NULL,
+    tradeVolume INTEGER NOT NULL,
     supply TEXT NOT NULL REFERENCES MarketTradeGoodSupply(value),
-    purchasePrice INT NOT NULL,
-    sellPrice INT NOT NULL
+    purchasePrice INTEGER NOT NULL,
+    sellPrice INTEGER NOT NULL
 );
 CREATE TABLE IF NOT EXISTS MarketTradeGoodSupply (
     value TEXT NOT NULL PRIMARY KEY
@@ -129,32 +129,32 @@ CREATE TABLE IF NOT EXISTS MarketTransaction (
     shipSymbol TEXT NOT NULL REFERENCES Ship(symbol),
     tradeSymbol TEXT NOT NULL REFERENCES TradeSymbol(value),
     type TEXT NOT NULL REFERENCES MarketTransactionType(value),
-    units INT NOT NULL,
-    pricePerUnit INT NOT NULL,
-    totalPrice INT NOT NULL,
-    timestamp INT NOT NULL
+    units INTEGER NOT NULL,
+    pricePerUnit INTEGER NOT NULL,
+    totalPrice INTEGER NOT NULL,
+    timestamp INTEGER NOT NULL
 );
 CREATE TABLE IF NOT EXISTS MarketTransactionType (
     value TEXT NOT NULL PRIMARY KEY
 );
 CREATE TABLE IF NOT EXISTS Ship (
     symbol TEXT NOT NULL PRIMARY KEY,
-    shipRegistration INT NOT NULL UNIQUE REFERENCES ShipRegistration(rowid),
-    shipNav INT NOT NULL UNIQUE REFERENCES ShipNav(rowid),
-    shipCrew INT UNIQUE REFERENCES ShipCrew(rowid),
-    shipFrame INT REFERENCES ShipFrame(rowid),
-    shipReactor INT REFERENCES ShipReactor(rowid),
-    shipEngine INT NOT NULL REFERENCES ShipEngine(rowid),
-    shipCargo INT NOT NULL UNIQUE REFERENCES ShipCargo(rowid),
-    fuelCurrent INT NOT NULL,
-    fuelCapacity INT NOT NULL,
-    fuelAmount INT,
-    fuelTimestamp INT,
+    shipRegistration INTEGER NOT NULL UNIQUE REFERENCES ShipRegistration(rowid),
+    shipNav INTEGER NOT NULL UNIQUE REFERENCES ShipNav(rowid),
+    shipCrew INTEGER UNIQUE REFERENCES ShipCrew(rowid),
+    shipFrame INTEGER REFERENCES ShipFrame(rowid),
+    shipReactor INTEGER REFERENCES ShipReactor(rowid),
+    shipEngine INTEGER NOT NULL REFERENCES ShipEngine(rowid),
+    shipCargo INTEGER NOT NULL UNIQUE REFERENCES ShipCargo(rowid),
+    fuelCurrent INTEGER NOT NULL,
+    fuelCapacity INTEGER NOT NULL,
+    fuelAmount INTEGER,
+    fuelTimestamp INTEGER,
 
-    shipFrame_condition INT,
-    shipReactor_Condition INT,
-    shipEngine_Condition INT NOT NULL,
-    lastEdited INT NOT NULL
+    shipFrame_condition INTEGER,
+    shipReactor_Condition INTEGER,
+    shipEngine_Condition INTEGER NOT NULL,
+    lastEdited INTEGER NOT NULL
 );
 CREATE TABLE IF NOT EXISTS Ship_ShipModules_relationship (
     ship TEXT NO NULL REFERENCES Ship(symbol),
@@ -165,26 +165,26 @@ CREATE TABLE IF NOT EXISTS Ship_ShipMounts_relationship (
     shipMount TEXT NOT NULL REFERENCES ShipMount(symbol)
 );
 CREATE TABLE IF NOT EXISTS ShipCargo (
-    capacity INT NOT NULL,
-    units INT NOT NULL
+    capacity INTEGER NOT NULL,
+    units INTEGER NOT NULL
 );
 CREATE TABLE IF NOT EXISTS ShipCargoItem (
     symbol TEXT NOT NULL,
     name TEXT NOT NULL,
     description TEXT NOT NULL,
-    units INT NOT NULL
+    units INTEGER NOT NULL
 );
 CREATE TABLE IF NOT EXISTS ShipCargo_ShipCargoItem_relationship (
-    shipCargoItem INT NOT NULL PRIMARY KEY REFERENCES ShipCargoItem(rowid),
-    shipCargo INT NOT NULL REFERENCES ShipCargo(rowid)
+    shipCargoItem INTEGER NOT NULL PRIMARY KEY REFERENCES ShipCargoItem(rowid),
+    shipCargo INTEGER NOT NULL REFERENCES ShipCargo(rowid)
 );
 CREATE TABLE IF NOT EXISTS ShipCrew (
-    "current" INT NOT NULL,
-    required INT NOT NULL,
-    capacity INT NOT NULL,
+    "current" INTEGER NOT NULL,
+    required INTEGER NOT NULL,
+    capacity INTEGER NOT NULL,
     rotation TEXT NOT NULL REFERENCES ShipCrewRotation(value),
-    morale INT NOT NULL,
-    wages INT NOT NULL
+    morale INTEGER NOT NULL,
+    wages INTEGER NOT NULL
 );
 CREATE TABLE IF NOT EXISTS ShipCrewRotation (
     value TEXT NOT NULL PRIMARY KEY
@@ -193,32 +193,32 @@ CREATE TABLE IF NOT EXISTS ShipEngine (
     symbol TEXT NOT NULL PRIMARY KEY,
     name TEXT NOT NULL,
     description TEXT NOT NULL,
-    speed INT NOT NULL,
-    requirements INT NOT NULL UNIQUE REFERENCES ShipRequirements(rowid)
+    speed INTEGER NOT NULL,
+    requirements INTEGER NOT NULL UNIQUE REFERENCES ShipRequirements(rowid)
 );
 CREATE TABLE IF NOT EXISTS ShipFrame (
     symbol TEXT NOT NULL PRIMARY KEY,
     name TEXT NOT NULL,
     description TEXT NOT NULL,
-    moduleSlots INT NOT NULL,
-    mountingPoints INT NOT NULL,
-    fuelCapacity INT NOT NULL,
-    requirements INT NOT NULL UNIQUE REFERENCES ShipRequirements(rowid)
+    moduleSlots INTEGER NOT NULL,
+    mountingPoints INTEGER NOT NULL,
+    fuelCapacity INTEGER NOT NULL,
+    requirements INTEGER NOT NULL UNIQUE REFERENCES ShipRequirements(rowid)
 );
 CREATE TABLE IF NOT EXISTS ShipModule (
     symbol TEXT NOT NULL PRIMARY KEY,
-    capacity INT,
-    range INT,
+    capacity INTEGER,
+    range INTEGER,
     name TEXT NOT NULL,
     description TEXT,
-    requirements INT NOT NULL UNIQUE REFERENCES ShipRequirements(rowid)
+    requirements INTEGER NOT NULL UNIQUE REFERENCES ShipRequirements(rowid)
 );
 CREATE TABLE IF NOT EXISTS ShipMount (
     symbol TEXT NOT NULL PRIMARY KEY,
     name TEXT NOT NULL,
 	description TEXT,
-    strength INT,
-    requirements INT NOT NULL REFERENCES ShipRequirements(rowid)
+    strength INTEGER,
+    requirements INTEGER NOT NULL REFERENCES ShipRequirements(rowid)
 );
 CREATE TABLE IF NOT EXISTS ShipMount_Deposits_relationship (
     shipMount TEXT NOT NULL REFERENCES ShipMount(symbol),
@@ -227,13 +227,13 @@ CREATE TABLE IF NOT EXISTS ShipMount_Deposits_relationship (
 CREATE TABLE IF NOT EXISTS ShipNav (
     systemSymbol TEXT NOT NULL REFERENCES System(symbol),
     waypointSymbol TEXT NOT NULL REFERENCES Waypoint(symbol),
-    route INT NOT NULL REFERENCES ShipNavRoute(rowid),
+    route INTEGER NOT NULL REFERENCES ShipNavRoute(rowid),
     status TEXT NOT NULL REFERENCES ShipNavStatus(value),
     flightMode TEXT NOT NULL REFERENCES ShipNavFlightMode(value),
-    route_destination INT NOT NULL REFERENCES Waypoint(rowid),
-    route_departure INT NOT NULL REFERENCES Waypoint(rowid),
-    route_departureTime INT NOT NULL,
-    route_arrival INT NOT NULL
+    route_destination INTEGER NOT NULL REFERENCES Waypoint(rowid),
+    route_departure INTEGER NOT NULL REFERENCES Waypoint(rowid),
+    route_departureTime INTEGER NOT NULL,
+    route_arrival INTEGER NOT NULL
 );
 CREATE TABLE IF NOT EXISTS ShipNavFlightMode (
     value TEXT NOT NULL PRIMARY KEY
@@ -245,8 +245,8 @@ CREATE TABLE IF NOT EXISTS ShipReactor (
     symbol TEXT NOT NULL PRIMARY KEY,
     name TEXT NOT NULL,
     description TEXT NOT NULL,
-    powerOutput INT NOT NULL,
-    requirements INT NOT NULL UNIQUE REFERENCES ShipRequirements(rowid)
+    powerOutput INTEGER NOT NULL,
+    requirements INTEGER NOT NULL UNIQUE REFERENCES ShipRequirements(rowid)
 );
 CREATE TABLE IF NOT EXISTS ShipRegistration(
     name TEXT NOT NULL,
@@ -254,9 +254,9 @@ CREATE TABLE IF NOT EXISTS ShipRegistration(
     role TEXT NOT NULL REFERENCES ShipRole(value)
 );
 CREATE TABLE IF NOT EXISTS ShipRequirements(
-    power INT,
-    crew INT,
-    slots INT
+    power INTEGER,
+    crew INTEGER,
+    slots INTEGER
 );
 CREATE TABLE IF NOT EXISTS ShipRole (
     value TEXT NOT NULL PRIMARY KEY
@@ -266,7 +266,7 @@ CREATE TABLE IF NOT EXISTS ShipType (
 );
 CREATE TABLE IF NOT EXISTS Shipyard (
     symbol TEXT NOT NULL PRIMARY KEY REFERENCES Waypoint(symbol),
-    lastEdited INT NOT NULL
+    lastEdited INTEGER NOT NULL
 );
 CREATE TABLE IF NOT EXISTS ShipYard_ShipType_relationships(
     shipYard TEXT NOT NULL REFERENCES Shipyard(symbol),
@@ -276,7 +276,7 @@ CREATE TABLE IF NOT EXISTS ShipyardShip (
     type TEXT REFERENCES ShipType(value),
     name TEXT NOT NULL,
     description TEXT NOT NULL,
-    purchasePrice INT NOT NULL,
+    purchasePrice INTEGER NOT NULL,
     frame TEXT NOT NULL REFERENCES ShipFrame(symbol),
     reactor TEXT NOT NULL REFERENCES ShipReactor(symbol),
     engine TEXT NOT NULL REFERENCES ShipEngine(symbol)
@@ -296,35 +296,35 @@ CREATE TABLE IF NOT EXISTS ShipyardShip_ShipMount_relationship (
 CREATE TABLE IF NOT EXISTS ShipyardTransaction (
     waypointSymbol TEXT NOT NULL REFERENCES Waypoint(symbol),
     shipSymbol TEXT NOT NULL REFERENCES Ship(symbol),
-    price INT NOT NULL,
+    price INTEGER NOT NULL,
     agentSymbol TEXT NOT NULL REFERENCES Agent(symbol),
-    timestamp INT NOT NULL
+    timestamp INTEGER NOT NULL
 );
 CREATE TABLE IF NOT EXISTS ShipyardTransaction_Shipyard_relationship (
-    shipyardTransaction INT NOT NULL PRIMARY KEY REFERENCES ShipyardTransaction(rowid),
+    shipyardTransaction INTEGER NOT NULL PRIMARY KEY REFERENCES ShipyardTransaction(rowid),
     shipyard TEXT NOT NULL REFERENCES Shipyard(symbol)
 );
 CREATE TABLE IF NOT EXISTS Survey (
     signature TEXT NO NULL,
     symbol TEXT NO NULL REFERENCES Waypoint(symbol),
-    expiration INT NO NULL,
+    expiration INTEGER NO NULL,
     size TEXT NO NULL CHECK (size IN ('SMALL','MODERATE','LARGE')),
-    lastEdited INT NOT NULL
+    lastEdited INTEGER NOT NULL
 );
 CREATE TABLE IF NOT EXISTS SurveyDeposit (
     symbol TEXT NO NULL PRIMARY KEY REFERENCES TradeSymbol(value)
 );
 CREATE TABLE IF NOT EXISTS SurveyDeposit_Survey_relationship (
-    deposit INT NO NULL PRIMARY KEY REFERENCES SurveyDeposit(rowid),
+    deposit INTEGER NO NULL PRIMARY KEY REFERENCES SurveyDeposit(rowid),
     survey TEXT NO NULL REFERENCES Survey(signature)
 );
 CREATE TABLE IF NOT EXISTS System (
     symbol TEXT NO NULL PRIMARY KEY,
     sectorSymbol TEXT NO NULL,
     type TEXT NO NULL REFERENCES SystemType(value),
-    x INT NO NULL,
-    y INT NO NULL,
-    lastEdited INT NOT NULL
+    x INTEGER NO NULL,
+    y INTEGER NO NULL,
+    lastEdited INTEGER NOT NULL
 );
 CREATE TABLE IF NOT EXISTS System_Faction_relationship(
     system TEXT NO NULL REFERENCES System(symbol),
@@ -345,10 +345,10 @@ CREATE TABLE IF NOT EXISTS Waypoint (
     symbol TEXT NOT NULL PRIMARY KEY,
     type TEXT NOT NULL,
     systemSymbol TEXT NOT NULL REFERENCES System(symbol),
-    x INT NOT NULL,
-    y INT NOT NULL,
+    x INTEGER NOT NULL,
+    y INTEGER NOT NULL,
     faction TEXT REFERENCES Faction(symbol),
-    lastEdited INT NOT NULL
+    lastEdited INTEGER NOT NULL
 );
 CREATE TABLE IF NOT EXISTS Orbital (
     symbol TEXT NO NULL PRIMARY KEY REFERENCES Waypoint(symbol),
