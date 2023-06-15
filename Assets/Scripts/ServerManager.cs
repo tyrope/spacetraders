566fc7275b69a5d96be8af1bbb67979dacef06e4
Tyrope <tyrope@tyrope.nl>
2023-06-15 02:07:52 +0200

    Merge Meta back into ServerManager to prepare for pagination.

--- Assets/Scripts/ServerManager.cs
+++ Assets/Scripts/ServerManager.cs
@@ -25,6 +25,15 @@ namespace STCommander
     }
     public class ServerManager
     {
+        public class Meta
+        {
+            public int total;
+            public int page;
+            public int limit;
+            public int TotalPages => Mathf.CeilToInt((float) total / (float) limit);
+
+            public override string ToString() => $"{Mathf.Min(total, limit)} result{(total > 1 ? "s" : "")} (Page {page}/{TotalPages})";
+        }
         private class ServerResponse<T>
         {
             public T data;
