81d9db17ec8fa626946ebdbe3c22aa3cf0fbccac
Tyrope <tyrope@tyrope.nl>
2023-06-16 15:56:07 +0200

    Merge lastrowid into writequery.

--- Assets/Scripts/DatabaseManager.cs
+++ Assets/Scripts/DatabaseManager.cs
@@ -11,11 +11,12 @@ namespace STCommander
     public class DatabaseManager : MonoBehaviour
     {
         public static DatabaseManager instance;
+        public static string RowIDQuery => "SELECT last_insert_rowid();";
 
         private readonly SqliteConnection sqlConnection = new SqliteConnection();
 
-        private readonly SqlLogVerbosity sendSqlToLog = SqlLogVerbosity.EVERYTHING; //TODO Sql Verbosity lives here.
-        private enum SqlLogVerbosity { NONE, ERROR_ONLY, WRITE_ONLY, WRITE_AND_SCALAR, EVERYTHING }
+        private enum SqlLogVerbosity { NONE, ERROR_ONLY, WRITE_NONSCALAR, WRITE_ALL, EVERYTHING }
+        private readonly SqlLogVerbosity sendSqlToLog = SqlLogVerbosity.WRITE_ALL; //TODO Sql Verbosity lives here.
 
         /// <summary>
         /// Generates an Connection object to the database, and if the database doesn't exist yet, creates it.
@@ -48,6 +49,7 @@ namespace STCommander
         }
 
         void OnDestroy() {
+            instance = null; // Idk if destroyed objects are nulled. Be explicit.
             sqlConnection.Dispose();
         }
 
@@ -93,13 +95,17 @@ namespace STCommander
             return returnValues;
         }
 
-        public async Task<int> WriteQuery( string query, CancellationToken cancel ) {
+        public async Task<int> WriteQuery( string query, CancellationToken cancel, bool requestingRowId = false ) {
             SqliteCommand command = new SqliteCommand(query);
             await sqlConnection.OpenAsync();
             command.Connection = sqlConnection;
             int result;
             try {
+                if(requestingRowId) {
+                    result = (int) (long) await command.ExecuteScalarAsync(cancel);
+                } else {
                 result = await command.ExecuteNonQueryAsync(cancel);
+                }
             } catch(SqliteException e) {
                 await command.DisposeAsync();
                 await sqlConnection.CloseAsync();
@@ -112,33 +118,13 @@ namespace STCommander
             await command.DisposeAsync();
             await sqlConnection.CloseAsync();
             if(cancel.IsCancellationRequested) { return default; } // Don't log or return anything useful on cancellations.
-            if(sendSqlToLog >= SqlLogVerbosity.WRITE_ONLY)
-                Debug.Log($"DatabaseManager::WriteQuery() -- Query parsed. ({result} rows)\n{query}");
-            return result;
-        }
-
-        public async Task<int> GetLatestRowid( CancellationToken cancel ) {
-            SqliteCommand command = new SqliteCommand("select last_insert_rowid()");
-            sqlConnection.Open();
-            command.Connection = (SqliteConnection) sqlConnection;
-            int result;
-            try {
-                long result64 = (long) await command.ExecuteScalarAsync();
-                result = (int) result64;
-            } catch(SqliteException e) {
-                await command.DisposeAsync();
-                sqlConnection.Close();
-                if(sendSqlToLog >= SqlLogVerbosity.ERROR_ONLY) {
-                    Debug.LogError("SQL Exception from grabbing last_insert_rowid()");
-                    Debug.LogException(e);
+            if(requestingRowId) {
+                if(sendSqlToLog >= SqlLogVerbosity.WRITE_ALL) {
+                    Debug.Log($"DatabaseManager::WriteQuery() -- Query parsed. (Row ID: {result})\n{query}");
                 }
-                return 0;
+            } else if(sendSqlToLog >= SqlLogVerbosity.WRITE_NONSCALAR) {
+                Debug.Log($"DatabaseManager::WriteQuery() -- Query parsed. ({result} rows)\n{query}");
             }
-            await command.DisposeAsync();
-            sqlConnection.Close();
-            if(cancel.IsCancellationRequested) { return default; }
-            if(sendSqlToLog >= SqlLogVerbosity.WRITE_AND_SCALAR)
-                Debug.Log($"DatabaseManager::GetLatestRowid() -- Success! {result}");
             return result;
         }
     }
